98bc2be78069c7f9abb6ecabaaca1f5f92687696
Ashitha T <ashithasl1996@gmail.com>
2021-11-30 23:41:35 +0530

    changes 1st

--- rent/rent/spiders/spider.py
+++ rent/rent/spiders/spider.py
@@ -9,19 +9,23 @@ class SpiderSpider(scrapy.Spider):
 
 
     def parse(self, response):
-         with open("out.json","w") as file:
-            file.write('[')
+         
          all_rents = response.xpath('//a[@aria-label="Listing link"]/@href').getall()
 
          for link in all_rents:
             yield scrapy.Request('https://www.bayut.com'+link,callback=self.parse1)
+
+    def parse2(self, response):
+         
+         nex = response.xpath('//a[@title="Next"]/@href').getall()
+
+         for link in nex:
+            yield scrapy.Request('https://www.bayut.com'+link,callback=self.parse1)     
             
-    def parse1(self, response):
+    def parse1(self, response): 
     
 
-            
-
-            reff_num=response.xpath('//li/span[@aria-label="Reference"]/text()').extract()[0]
+            reff_num=response.xpath('//span[@aria-label="Reference"]/text()').extract()[0]
 
             purpose=response.xpath('//li/span[@aria-label="Purpose"]/text()').extract()[0]
 
@@ -35,39 +39,42 @@ class SpiderSpider(scrapy.Spider):
 
             location=response.xpath('//div[@aria-label="Property header"]/text()').extract()[0]
 
-            bed_bath_size=response.xpath('//div[@class="ba1ca68e _0ee3305d"]').extract()[0]
+            bed_bath_size= response.xpath('//div[@class="_6f6bb3bc"]//text()').extract()
+
 
             permit_number=response.xpath('//div/span[@class="ff863316"]/text()').extract()[0]
 
             agent_name=response.xpath('//div/span[@aria-label="Agent name"]/text()').extract()[0]
 
-            #image_url=self.start_urls[0] + ','.join(response.xpath('//img[@title="1 "]/@src')).extract()[0]
+            image_url=self.start_urls[0] + ','.join(response.xpath('//div[@class="a8e3563c"]/@src'))
 
-            breadcrumbs=response.xpath('//div[@aria-label="Breadcrumb"]').extract()
+            breadcrumbs=response.xpath('//div[@aria-label="Breadcrumb"]//text()').extract()[0]
 
-            amenities=response.xpath('//h3[@class="_9676c577"]').extract()
 
-            description=response.xpath('//div/span[@aria-label="Property description"]').extract()
+            amenities=response.xpath('//div[@class="ef5bd664"]//text()').extract()
+
+            description=response.xpath('//div[@aria-label="Property description"]//text()').extract()[0]
+ 
         
             yield {
               
-              'reff_num':reff_num,
+              'ref_num':reff_num,
               'purpose':purpose,
               'type_r':type_r,
               'added on':added_on,
               'furnishing':furnishing,
               'Price':Price,
               'location':location,
-              #'bed_bath_size':bed_bath_size,
+              'bed_bath_size':bed_bath_size,
               'permit_number':permit_number,
               'agent_name':agent_name,
-              #'image_url':image_url,
+              'image_url':image_url,
               'breadcrumbs':breadcrumbs,
               'amenities':amenities,
               'description':description,
              }
 
-         
+    
 
 
 
