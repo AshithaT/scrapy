9dbe8a2172f961e28bf9e7321c8cdab2ac20ceca
Ashitha T <ashithasl1996@gmail.com>
2021-12-02 16:14:29 +0530

    changes 4th

--- rent/rent/spiders/spider.py
+++ rent/rent/spiders/spider.py
@@ -14,8 +14,14 @@ class SpiderSpider(scrapy.Spider):
 
         for link in all_rents:
             yield scrapy.Request('https://www.bayut.com'+link,callback=self.parse1)
-        
-        
+
+        Next = response.xpath('//a[@title="Next"]/@href').get()
+            
+        if Next is not None:
+                  pg_url = f"https://www.bayut.com{Next}"
+                  request=scrapy.Request(url=pg_url,callback=self.parse1) 
+                  yield request 
+             
             
     def parse1(self, response): 
     
@@ -30,11 +36,17 @@ class SpiderSpider(scrapy.Spider):
 
             furnishing=response.xpath('//li/span[@aria-label="Furnishing"]/text()').extract()
 
-            Price =response.xpath('//div/span[@aria-label="Price"]/text()').extract()[0]
+            Price =response.xpath('//div/span[@aria-label="Price"]//text()').extract()
+
+            currency=response.xpath('//div/span[@aria-label="Currency"]/text()').extract()
 
             location=response.xpath('//div[@aria-label="Property header"]/text()').extract()[0]
 
-            bed_bath_size= response.xpath('//div[@class="_6f6bb3bc"]//text()').extract()
+            bed= response.xpath('//span[@aria-label="Beds"]//text()').extract()
+
+            bath=response.xpath('//span[@aria-label="Baths"]//text()').extract()
+
+            size=response.xpath('//span[@aria-label="Area"]//text()').extract()
 
             permit_number=response.xpath('//div/span[@class="ff863316"]/text()').extract()[0]
 
@@ -51,28 +63,28 @@ class SpiderSpider(scrapy.Spider):
             
             yield {
               
-              'ref_num':reff_num,
-              'purpose':purpose,
-              'type_r':type_r,
-              'added on':added_on,
-              'furnishing':furnishing,
-              'Price':Price,
-              'location':location,
-              'bed_bath_size':bed_bath_size,
-              'permit_number':permit_number,
-              'agent_name':agent_name,
-              'image_url':image_url,
-              'breadcrumbs':breadcrumbs,
-              'amenities':amenities,
-              'description':description,
+              # 'ref_num':reff_num,
+              # 'purpose':purpose,
+              # 'type_r':type_r,
+              # 'added on':added_on,
+              # 'furnishing':furnishing,
+              'Price':{'Price':Price,
+                       'currency':currency},
+              # 'location':location,
+              'bed_bath_size':{'bed' :bed,
+                               'bath':bath,
+                               'size':size},
+                                          
+              # 'permit_number':permit_number,
+              # 'agent_name':agent_name,
+              # 'image_url':image_url,
+              # 'breadcrumbs':breadcrumbs,
+              # 'amenities':amenities,
+              # 'description':description,
 
-             }
-            Next = response.xpath('//a[@title="Next"]/@href').get()
+            }
             
-            if Next is not None:
-               pg_url = f"https://www.bayut.com{Next}"
-               request=scrapy.Request(url=pg_url,callback=self.parse1) 
-               yield request   
+             
 
             
 
